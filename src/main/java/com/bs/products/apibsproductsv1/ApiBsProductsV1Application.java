8fb13cd32a5f52bfd639b6f2b3a0e4ad0a5ede83
Jose Rosas <jose_alonso_rosas@outlook.com>
2021-11-03 23:09:32 -0500

    Initial Commit

---
+++ src/main/java/com/bs/products/apibsproductsv1/ApiBsProductsV1Application.java
@@ -0,0 +1,13 @@
+package com.bs.products.apibsproductsv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBsProductsV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBsProductsV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bs/products/apibsproductsv1/ApiBsProductsV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.products.apibsproductsv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBsProductsV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
